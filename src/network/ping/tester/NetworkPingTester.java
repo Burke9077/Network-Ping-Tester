5b60be9c22ed47ddc56a64d699af0e5868a8692e
Burke9077 <burke9077@gmail.com>
2013-12-29 14:45:33 -0500

    Initial Project Setup

---
+++ src/network/ping/tester/NetworkPingTester.java
@@ -0,0 +1,15 @@
+package network.ping.tester;
+
+/**
+ *
+ * @author Matthew Larry Burke
+ */
+public class NetworkPingTester {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+}
